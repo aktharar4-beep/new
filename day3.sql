e85d6afafd7fb052c64c446e3414e24166b2271a
AR <aktharar4@gmail.com>
2026-05-05 14:32:37 +0530

    add initial SQL script for store data insertion and queries

---
+++ day3.sql
@@ -0,0 +1,6 @@
+INSERT INTO store VALUES(1,'surf','soap',200,'yes'),(2,'lifeboy','soap',400,'no'),(3,'surf','paste',600,'yes'),(4,'closeup','paste',800,'no');
+SELECT * FROM store WHERE in_stock='yes';
+SELECT * FROM store WHERE price<500 AND in_stock='yes';
+SELECT * FROM store WHERE price>1000 AND in_stock='no';
+SELECT name,price FROM store ORDER BY price DESC;
+SELECT name,price+(price * 10 /100) AS price_with_tax FROM store;
